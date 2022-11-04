b2466c03dc5791cd88bf84ca7a117a2a58c14145
Ellis <ellisgraham97@hotmail.co.uk>
2022-11-04 17:14:30 +0000

    Java Swing Practice
    Quick Java practice creating a simple button

--- src/library/Login.java
+++ src/library/Login.java
@@ -4,9 +4,18 @@ import javax.swing.*;
 
 public class Login {
 
-    public static void main(String[] args) {
-         
-
-  }
-    
+	public static void main(String[] args) {  
+		JFrame f=new JFrame();	          
+		JButton b=new JButton("click");  
+		b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+		          
+		f.add(b);//adding button in JFrame  
+		          
+		f.setSize(400,500);
+		f.setLayout(null);//using no layout managers  
+		f.setVisible(true);//making the frame visible  
+		}  	
 }
+
+
+
