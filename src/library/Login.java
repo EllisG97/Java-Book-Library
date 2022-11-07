8268c9300241bccd34b243c2276059f7916f7d89
Ellis <ellisgraham97@hotmail.co.uk>
2022-11-07 15:29:09 +0000

    Added Dummy Login UI
    Added a dummy login UI for registering a new user and logging in to the system

--- src/library/Login.java
+++ src/library/Login.java
@@ -9,12 +9,39 @@ public class Login {
 		MySQLAccess dao = new MySQLAccess();
 		dao.readDataBase();
 		
-		 JFrame f=new JFrame(); JButton b=new JButton("click");
-		 b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+		 JFrame f=new JFrame(); 
+		 JButton login=new JButton("Login");
+		 JTextField username;
+		 JPasswordField password;
+		 JLabel nameLabel, passLabel, welcome;
 		 
-		 f.add(b);//adding button in JFrame
+		 nameLabel = new JLabel("Username: ");
+		 passLabel = new JLabel("Password: ");
+		 welcome = new JLabel("Welcome to Castle Rock Library!");
+		 
+		 username = new JTextField(" ");
+		 password = new JPasswordField("");
+		 
+		 JButton register = new JButton("Register");
+		 
+		 welcome.setBounds(250, 15, 300, 30);
+		 login.setBounds(300,200,100, 40);//x axis, y axis, width, height
+		 username.setBounds(270, 100, 200, 30);
+		 nameLabel.setBounds(170, 100, 100, 30);
+		 passLabel.setBounds(170, 150, 100, 30);
+		 password.setBounds(270, 150, 200, 30);
+		 register.setBounds(300, 300, 100, 40);
+		 
+		 f.add(login);//adding button in JFrame
+		 f.add(register);
+		 f.add(username);
+		 f.add(password);
+		 f.add(nameLabel);
+		 f.add(passLabel);
+		 f.add(welcome, SwingConstants.CENTER);
 
-		 f.setSize(400,500); f.setLayout(null);//using no layout managers
+		 f.setSize(750,500); 
+		 f.setLayout(null);//using no layout managers
 		 f.setVisible(true);//making the frame visible
-		  }
+	}
 }
